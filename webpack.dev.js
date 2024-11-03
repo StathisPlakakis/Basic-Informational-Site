df5a42b9775b7bd055c78dc60f6d8964834ddca9
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-11-03 19:16:20 +0200

    Build the project

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,16 @@
 const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+const common = require('./webpack.common.js');
+const path = require('path');
 
- module.exports = merge(common, {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   devServer: {
-     static: './dist',
-   },
- });
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000, // Or any port you prefer
+    open: true, // Automatically open the browser
+  },
+});
